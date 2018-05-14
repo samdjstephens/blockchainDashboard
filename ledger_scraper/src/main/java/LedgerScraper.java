118df3a11279320d2d0b9e8cf709dfd4ee3e98fb
Sam Stephens <samdjstephens@gmail.com>
2018-05-14 15:46:58 +0100

    Dockerfile

--- ledger_scraper/src/main/java/LedgerScraper.java
+++ ledger_scraper/src/main/java/LedgerScraper.java
@@ -1,6 +1,7 @@
 import java.net.URI;
 import java.net.URISyntaxException;
 
+
 public class LedgerScraper {
     public static void main(String[] args) throws URISyntaxException {
         BitcoinTransactionScraper scraper = new BitcoinTransactionScraper(new URI("wss://ws.blockchain.info/inv"));
